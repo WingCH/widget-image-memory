b08e84dedac3fb04fcca0b71feae141b27a11ab7
WingCH <wingch.apps@gmail.com>
2021-09-15 19:15:20 +0800

    Initial Commit

---
+++ widget-image-memory/widget_image_memoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  widget_image_memoryApp.swift
+//  widget-image-memory
+//
+//  Created by WingCH on 15/9/2021.
+//
+
+import SwiftUI
+
+@main
+struct widget_image_memoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
